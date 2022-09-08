0f559e3a931b9791d6870866c3d3b2b97def368c
jonathan briceño <brimanz2014@gmail.com>
2022-09-08 18:20:05 -0400

    curse part 2

---
+++ src/app/dbz/main-page/main-page.component.ts
@@ -0,0 +1,30 @@
+import { Component} from '@angular/core';
+
+
+interface Personaje{
+  nombre: string;
+  poder : number;
+}
+
+
+@Component({
+  selector: 'app-main-page',
+  templateUrl: 'main-page.component.html'
+})
+export class MainPageComponent{
+
+  nuevo: Personaje = {
+    nombre: "Trunks",
+    poder: 14000
+  }
+
+  cambiarNombre(event: any){
+    console.log(event.target.value)
+  }
+
+
+
+  agregar(){
+    console.log(this.nuevo);
+  }
+}
